2abd327e9638286cd780d06939ceb9c1b14be7fe
Reed Law <reed@doctible.com>
2019-04-17 12:55:21 -0700

    remove default timezone option and make it an argument

--- lib/cronex/exp_descriptor.rb
+++ lib/cronex/exp_descriptor.rb
@@ -12,19 +12,19 @@ module Cronex
     verbose: false,
     zero_based_dow: true,
     use_24_hour_time_format: false,
-    throw_exception_on_parse_error: true,
-    timezone: 'UTC'
+    throw_exception_on_parse_error: true
   }
 
   class ExpressionDescriptor
     attr_accessor :expression, :expression_parts, :options, :parsed, :resources
 
-    def initialize(expression, options = {}, locale = nil)
+    def initialize(expression, options = {}, locale = nil, timezone = 'UTC')
       @expression = expression
       @options = CRONEX_OPTS.merge(options)
       @expression_parts = []
       @parsed = false
       @resources = Cronex::Resource.new(locale)
+      @timezone = timezone
     end
 
     def to_hash
@@ -130,19 +130,19 @@ module Cronex
       description = ''
       if [sec_exp, min_exp, hour_exp].all? { |exp| !Cronex::Utils.include_any?(exp, SPECIAL_CHARS) }
         # specific time of day (i.e. 10 14)
-        description += resources.get('at') + ' ' + Cronex::Utils.format_time(hour_exp, min_exp, sec_exp, options[:timezone])
+        description += resources.get('at') + ' ' + Cronex::Utils.format_time(hour_exp, min_exp, sec_exp, @timezone)
       elsif min_exp.include?('-') && !min_exp.include?('/') && !min_exp.include?(',') && !Cronex::Utils.include_any?(hour_exp, SPECIAL_CHARS)
         # Minute range in single hour (e.g. 0-10 11)
         min_parts = min_exp.split('-')
         description += format(
           resources.get('every_minute_between'),
-          Cronex::Utils.format_time(hour_exp, min_parts[0], '', options[:timezone]),
-          Cronex::Utils.format_time(hour_exp, min_parts[1], '', options[:timezone]))
+          Cronex::Utils.format_time(hour_exp, min_parts[0], '', @timezone),
+          Cronex::Utils.format_time(hour_exp, min_parts[1], '', @timezone))
       elsif hour_exp.include?(',') && !Cronex::Utils.include_any?(min_exp, SPECIAL_CHARS)
         # Hours list with single minute (e.g. 30 6,14,16)
         hour_parts = hour_exp.split(',')
         description += resources.get('at')
-        h_parts = hour_parts.map { |part| ' ' + Cronex::Utils.format_time(part, min_exp, '', options[:timezone]) }
+        h_parts = hour_parts.map { |part| ' ' + Cronex::Utils.format_time(part, min_exp, '', @timezone) }
         description += h_parts[0...-1].join(',') + ' ' + resources.get('and') + h_parts.last
       else
         sec_desc = seconds_description(expression_parts)
--- spec/exp_descriptor_en_spec.rb
+++ spec/exp_descriptor_en_spec.rb
@@ -4,8 +4,8 @@ require 'cronex'
 module Cronex
   describe ExpressionDescriptor do
 
-    def desc(expression, opts = {})
-      Cronex::ExpressionDescriptor.new(expression, opts, 'en').description
+    def desc(expression, opts = {}, timezone = 'UTC')
+      Cronex::ExpressionDescriptor.new(expression, opts, 'en', timezone).description
     end
 
     let(:opts) { { zero_based_dow: false } }
@@ -382,22 +382,14 @@ module Cronex
     context 'timezone' do
       it 'minute span' do
         tz = TZInfo::Timezone.get('America/Los_Angeles')
-        result = if tz.period_for_local(Time.now).zone_identifier.to_s == 'PDT'
-                   'Every minute between 4:00 AM and 4:10 AM'
-                 else
-                   'Every minute between 3:00 AM and 3:10 AM'
-                 end
-        expect(desc('0-10 11 * * *', timezone: 'America/Los_Angeles')).to eq(result)
+        hour = tz.period_for_local(Time.now).zone_identifier.to_s == 'PDT' ? 4 : 3
+        expect(desc('0-10 11 * * *', {}, 'America/Los_Angeles')).to eq("Every minute between #{hour}:00 AM and #{hour}:10 AM")
       end
 
       it 'twice a day' do
         tz = TZInfo::Timezone.get('America/Los_Angeles')
-        result = if tz.period_for_local(Time.now).zone_identifier.to_s == 'PDT'
-                   'At 5:00 PM and 5:00 AM'
-                 else
-                   'At 4:00 PM and 4:00 AM'
-                 end
-        expect(desc('0 0,12 * * *', timezone: 'America/Los_Angeles')).to eq(result)
+        hour = tz.period_for_local(Time.now).zone_identifier.to_s == 'PDT' ? 5 : 4
+        expect(desc('0 0,12 * * *', {}, 'America/Los_Angeles')).to eq("At #{hour}:00 PM and #{hour}:00 AM")
       end
     end
   end
